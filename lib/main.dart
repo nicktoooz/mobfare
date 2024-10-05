146e5ed5f859f3168a32fe278f43f93b8367d8af
Dominic Mendoza Enriquez <dome.enriquez.up@phinmaed.com>
2024-10-05 11:35:15 +0800

    Foo

--- lib/main.dart
+++ lib/main.dart
@@ -37,16 +37,21 @@ class _HomePageState extends State<HomePage> {
     'Dagupan': {'Urdaneta': 58, 'Calasiao': 20},
     'Calasiao': {'Dagupan': 20, 'Urdaneta': 40},
   };
-
   @override
   void initState() {
     super.initState();
     vehicles = [
+      FeaturedVehicles(Vehicle(
+        'tricycle',
+        "Tricycle",
+        'assets/image/tricycle.png',
+      )),
       FeaturedVehicles(
-          Vehicle('tricycle', "Tricycle", '/assets/images/tricycle.png')),
-      FeaturedVehicles(Vehicle('bus', "Bus", '/assets/images/bus.png')),
+        Vehicle('bus', "Bus", 'assets/image/bus.png'),
+      ),
       FeaturedVehicles(
-          Vehicle('jeepney', "Jeepney", '/assets/images/jeepney.png')),
+        Vehicle('jeepney', "Jeepney", 'assets/image/jeepney.png'),
+      )
     ];
   }
 
@@ -91,9 +96,13 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget FeaturedVehicles(Vehicle vhc) {
+  Widget FeaturedVehicles(
+    Vehicle vhc,
+  ) {
     final TextEditingController _fromController = TextEditingController();
     final TextEditingController _toController = TextEditingController();
+    int payment = 0;
+    String change = '';
     String choices = 'regular';
     return GestureDetector(
       onTap: () {
@@ -101,7 +110,14 @@ class _HomePageState extends State<HomePage> {
           context: context,
           builder: (context) {
             List<String> cities = ['Urdaneta', 'Dagupan', 'Calasiao'];
-            String fareResult = 'FOOO';
+            String fareResult = '';
+            void changeBackgroundColour(StateSetter updateState, String _type) {
+              updateState(
+                () {
+                  choices = _type;
+                },
+              );
+            }
 
             void computeFare(StateSetter updateState, String type) {
               String fromCity = _fromController.text;
@@ -119,6 +135,12 @@ class _HomePageState extends State<HomePage> {
                   updateState(() {
                     fareResult =
                         '${choices.replaceFirst(choices[0], choices[0].toUpperCase())}: $fromCity -> $toCity : ₱$adjustedFare';
+                    if (payment > 0 && (payment - adjustedFare) > 0) {
+                      change = 'Change: ${(payment - adjustedFare)}';
+                    } else {
+                      change = 'Insufficient payment';
+                    }
+                    ;
                   });
                 } else {
                   updateState(() {
@@ -134,10 +156,20 @@ class _HomePageState extends State<HomePage> {
               content: StatefulBuilder(
                 builder: (context, setStateDialog) {
                   return SizedBox(
-                    height: 250,
+                    height: 350,
                     child: Column(
                       children: [
                         TypeAheadField(
+                          builder: (context, controller, focusNode) {
+                            return TextFormField(
+                              controller: controller,
+                              focusNode: focusNode,
+                              decoration: const InputDecoration(
+                                hintText: "Origin",
+                                border: OutlineInputBorder(),
+                              ),
+                            );
+                          },
                           controller: _fromController,
                           suggestionsCallback: (pattern) {
                             computeFare(setStateDialog, choices);
@@ -159,11 +191,21 @@ class _HomePageState extends State<HomePage> {
                         ),
                         const SizedBox(height: 10),
                         TypeAheadField(
+                          builder: (context, controller, focusNode) {
+                            return TextFormField(
+                              controller: controller,
+                              focusNode: focusNode,
+                              decoration: const InputDecoration(
+                                hintText: "Destination",
+                                border: OutlineInputBorder(),
+                              ),
+                            );
+                          },
                           controller: _toController,
                           suggestionsCallback: (pattern) {
                             computeFare(setStateDialog, choices);
                             return cities
-                                .where((element) => element
+                                .where((city) => city
                                     .toLowerCase()
                                     .contains(pattern.toLowerCase()))
                                 .toList();
@@ -178,7 +220,36 @@ class _HomePageState extends State<HomePage> {
                             computeFare(setStateDialog, choices);
                           },
                         ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                            onChanged: (val) {
+                              payment = int.parse(val);
+                              computeFare(setStateDialog, choices);
+                            },
+                            decoration: const InputDecoration(
+                              hintText: "Payment",
+                              border: OutlineInputBorder(),
+                            )),
                         const SizedBox(height: 20),
+                        Text(
+                          fareResult,
+                          style: TextStyle(
+                            color: fareResult.contains('not available')
+                                ? Colors.red
+                                : Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          change,
+                          style: TextStyle(
+                            color: fareResult.contains('not available')
+                                ? Colors.red
+                                : Colors.black,
+                          ),
+                        ),
                         Expanded(
                           child: Row(
                             spacing: 20,
@@ -187,36 +258,77 @@ class _HomePageState extends State<HomePage> {
                               GestureDetector(
                                 onTap: () {
                                   choices = 'regular';
+                                  changeBackgroundColour(
+                                      setStateDialog, 'regular');
                                   computeFare(setStateDialog, choices);
                                 },
                                 child: Container(
-                                    color: Colors.blue, height: 50, width: 50),
+                                  height: 50,
+                                  padding: EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.blue,
+                                      ),
+                                      color: choices == 'regular'
+                                          ? Colors.blue.shade100
+                                          : Colors.transparent,
+                                      borderRadius: BorderRadius.circular(8)),
+                                  width: 50,
+                                  child: Center(
+                                    child:
+                                        Image.asset('assets/image/regular.png'),
+                                  ),
+                                ),
                               ),
                               GestureDetector(
                                 onTap: () {
                                   choices = 'student';
+                                  changeBackgroundColour(setStateDialog, 'pwd');
                                   computeFare(setStateDialog, choices);
                                 },
                                 child: Container(
-                                    color: Colors.blue, height: 50, width: 50),
+                                  height: 50,
+                                  padding: EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.blue,
+                                      ),
+                                      color: choices == 'pwd'
+                                          ? Colors.blue.shade100
+                                          : Colors.transparent,
+                                      borderRadius: BorderRadius.circular(8)),
+                                  width: 50,
+                                  child: Center(
+                                    child: Image.asset('assets/image/pwd.png'),
+                                  ),
+                                ),
                               ),
                               GestureDetector(
                                 onTap: () {
                                   choices = 'pwd';
+                                  changeBackgroundColour(
+                                      setStateDialog, 'student');
                                   computeFare(setStateDialog, choices);
                                 },
                                 child: Container(
-                                    color: Colors.blue, height: 50, width: 50),
+                                  height: 50,
+                                  padding: EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.blue,
+                                      ),
+                                      color: choices == 'student'
+                                          ? Colors.blue.shade100
+                                          : Colors.transparent,
+                                      borderRadius: BorderRadius.circular(8)),
+                                  width: 50,
+                                  child: Center(
+                                    child:
+                                        Image.asset('assets/image/student.png'),
+                                  ),
+                                ),
                               ),
                             ],
-                          ),
-                        ),
-                        Text(
-                          fareResult,
-                          style: TextStyle(
-                            color: fareResult.contains('not available')
-                                ? Colors.red
-                                : Colors.black,
                           ),
                         ),
                       ],
@@ -248,14 +360,17 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(
+            SizedBox(
               height: 180,
               width: double.infinity,
-              child: Placeholder(),
+              child: Image.asset(vhc.image),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Text(vhc.name),
+              child: Text(
+                vhc.name,
+                style: const TextStyle(fontSize: 24),
+              ),
             ),
           ],
         ),
@@ -263,7 +378,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  // Base fare calculation function
   int? calculateBaseFare(String from, String to) {
     if (baseFareData.containsKey(from) && baseFareData[from]!.containsKey(to)) {
       return baseFareData[from]![to];
